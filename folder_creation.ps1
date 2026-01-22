0e0c8867442a21c86a223c639903487029bb249b
Darshana K <darshanak884@gmail.com>
2026-01-22 10:23:44 +0530

    Delete folder_creation.ps1

--- folder_creation.ps1
+++
@@ -1,8 +0,0 @@
-$path = "C:\AutomationLab"
-
-if (!(Test-Path $path)) {
-    New-Item -ItemType Directory -Path $path
-    Write-Output "Folder 'AutomationLab' created successfully."
-} else {
-    Write-Output "Folder 'AutomationLab' already exists."
-}
