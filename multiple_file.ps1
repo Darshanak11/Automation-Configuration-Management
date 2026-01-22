dfce813f81989728a23252f4784112f7087241d4
darshana <darshanak884@gmail.com>
2026-01-22 10:27:18 +0530

    Added files

---
+++ multiple_file.ps1
@@ -0,0 +1,11 @@
+$folderPath = "C:\AutomationLab"
+$numberOfFiles = 5
+if (!(Test-Path $folderPath)) {
+    New-Item -ItemType Directory -Path $folderPath
+}
+
+# Loop to create files
+for ($i = 1; $i -le $numberOfFiles; $i++) {
+    $filePath = "$folderPath\data$i.txt"
+    "This is data file number $i" | Out-File $filePath
+}
