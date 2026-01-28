f33871ba037473625858160e8bc6696e85ff5005
Darshana K <darshanak884@gmail.com>
2026-01-28 18:32:12 +0530

    Delete folder creation.ps1

--- folder creation.ps1
+++
@@ -1,8 +0,0 @@
-$path = "C:\Users\darzn\OneDrive\Desktop\Github Repos\Automation-Configuration-Management\ACM-Lab"
-
-if (!(Test-Path $path)) {
-    New-Item -ItemType Directory -Path $path
-    Write-Output "Folder 'AutomationLab' created successfully."
-} else {
-    Write-Output "Folder 'AutomationLab' already exists."
-}
